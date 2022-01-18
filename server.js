ec9cd1199b7439d6ad97a34e595b5a5a0d79e6f6
l bates childs <labchilds@gmail.com>
2022-01-17 19:44:02 -0500

    add CRUD routes

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -19,36 +20,86 @@ const db = mysql.createConnection(
     console.log('Connected to the election database.')
 );
 
-/* db.query(`SELECT * FROM candidates`, (err, rows) => {
-    console.log(rows);
-}); */
+// get all candidates
+app.get('/api/candidates', (req, res) => {
+    const sql = `SELECT * FROM candidates`;
 
-// get a single candidate
-/* db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
-    if (err) {
-        console.log (err);
-    }
-    console.log(row);
-}); */
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        }
+        res.json({
+            massage: 'success',
+            data: rows
+        });
+    });
+});
 
-// delete a candidate
-/* db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
-    if (err) {
-        console.log(err);
-    }
-    console.log(result);
-}); */
+//get a single candidate
+app.get('/api/candidate/:id', (req, res) => {
+    const sql = `SELECT * FROM candidates WHERE id = ?`;
+    const params = [req.params.id]; // this is where we'll get id to replace ? placeholder
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json( { error: err.message }) // an obj with error
+            return; // stop the function
+        }
+        res.json({
+            message: 'success',
+            data: row
+        });
+    });
+});
+
+app.delete('/api/candidate/:id', (req, res) => {
+    const sql = `DELETE FROM candidates WHERE id = ?`;
+    const params = [req.params.id];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            req.statusMessage(400).json({ error: res.message });
+        } else if (!result.affectedRows) {
+            res.json({
+                message: 'Candidate not found.'
+            });
+        } else {
+            res.json({
+                message: 'deleted',
+                changes: result.affectedRows,
+                id: req.params.id
+            });
+        }
+    });
+});
 
 // create a candidate
-/* const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected) VALUES (?,?,?,?)`;
-const params = [1, 'Ronald', 'Firbank', 1];
+app.post('/api/candidate', ({ body }, res) => { // use destructuring to pull out body prop from req obj
+    // use our validation ulit (takes obj and ...props as arguments)
+    // always validate before sending to db!! keep your db clean of erroneous data! (and save moeny nothing free)
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
 
-db.query(sql, params, (err, result) => {
-    if (err) {
-        console.log(err);
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
     }
-    console.log(result);
-}); */
+
+    // error free? continue to the db!
+    const sql = `INSERT INTO candidates ( first_name, last_name, industry_connected) VALUES (?,?,?)`
+    const params = [body.first_name, body.last_name, body.industry_connected]; // don't need id bc sql will auto_increment for us
+    
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
+    });
+});
 
 // default response for any other request
 app.use((req, res) => {
